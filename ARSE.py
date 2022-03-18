fa0287bb2c2b9f007988f41d8c12a9db95330fc4
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2022-03-18 07:35:54 +0100

    Add files via upload

--- ARSE.py
+++ ARSE.py
@@ -1,5 +1,5 @@
 """
-Created 21. January 2020 by Daniel Van Opdenbosch, Technical University of Munich
+Created 19. May 2020 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -51,12 +51,7 @@ def TM(params):
 		sim=tmm.inc_tmm('s',nk_list,d_list,c_list,0+0j,l)
 		Tsim=numpy.append(Tsim,sim['T'])
 		Rsim=numpy.append(Rsim,sim['R'])
-	global res
-	res=Tmeas-(Tsim-Ssim)
-	global Rquadrat
-	Rquadrat=1-numpy.var(res)/numpy.var(Tmeas)
-	# ~ print(Rquadrat)
-	return res
+	return Tmeas-(Tsim-Ssim)
 
 #Einlesen
 files=glob.glob('*_c01_*.txt')
@@ -96,32 +91,14 @@ for i in files:
 	params.add('Cs',1,min=0,max=1000)
 	params.add('alpha',1,min=0,max=2)
 
-	#Vorgeschaltete globale Loesungssuche
-	lamb=lamb0[argsred]
-	Tmeas=Tmeas0[argsred]
-	result0=lmfit.minimize(TM,params,method='differential_evolution')
-
-	params=lmfit.Parameters()
-	prm0=result0.params.valuesdict()
-	params.add('d',prm0['d'],vary=False)
-	params.add('A',prm0['A'])
-	params.add('B1',prm0['B1'])
-	params.add('C1',prm0['C1'])
-	params.add('Cd',prm0['Cd'])
-	params.add('Cs',prm0['Cs'])
-	params.add('alpha',prm0['alpha'])
-	#Bis hierher ausschalten
-
 	lamb=lamb0[args]
 	Tmeas=Tmeas0[args]
 	result=lmfit.minimize(TM,params,method='least_squares')
-
-	print('R^2:',Rquadrat)
 	result.params.pretty_print()
 
 	#Speichern und laden
-	pickle.dump(result,open(str(filename)+'_result','wb'))
-	result=pickle.load(open(str(filename)+'_result','rb'))
+	numpy.save(str(filename)+'_result',result)
+	result=pickle.load(str(filename)+'_result')
 
 	#Sammeln
 	prm=result.params.valuesdict()
@@ -163,14 +140,31 @@ for i in files:
 	plt.savefig(filename+'_nk_collect.pdf')
 	plt.savefig(filename+'_nk_collect.png')
 
-	#Exportieren
-	export=numpy.array([lamb,Tmeas,Tsim,Ssim,n_collect,k_collect])
-	numpy.savetxt(filename+'.tsv',export.transpose(),delimiter='\t',newline='\n')
+#Exportieren
+export=numpy.array([lamb,Tmeas,Tsim,Ssim,n_collect,k_collect])
 
-f=open('Fitparameter.csv','w')
-f.write('Dateiname,d_collect+-unc,A_collect+-unc,B1_collect+-unc,C1_collect+-unc,c_collect+-unc,alpha_collect+-unc\n')
-for i,filename in enumerate(filenames_collect):
-	f.write(filename)
-	for j in [d_collect[i],A_collect[i],B1_collect[i],C1_collect[i],Cd_collect[i],Cs_collect[i],alpha_collect[i]]:
-		f.write(','+str(float(j.magnitude))+'+-'+str(float(j.uncertainty)))
-	f.write('\n')
+# ~ print(export)
+
+def namestr(obj, namespace):
+	return str([name for name in namespace if namespace[name] is obj][0])
+exportstring=str()
+for j,value in enumerate(export):
+	if j!=0:
+		exportstring+=','
+	exportstring+=namestr(export[j],locals())
+
+pickle.dump(export,open('data.pic','wb'))
+
+print('____')
+print('Ausgegeben als Liste von Python quantities.UncertainQuantity: ['+exportstring+']')
+print('____')
+print("Zum Laden der Liste: pickle.load(open(filenamepattern+'.pic','rb')")
+
+sys.stdout=open(filenamepattern.replace('*','alle')+'.txt','w')
+for i,valuei in enumerate(filenamelist):
+	printline=str(files[i])
+	for j,valuej in enumerate(export):
+		if j>1 and valuej!=[]:
+			printline+='; '+namestr(export[j],locals())+': '+str(float(valuej[i].magnitude))+' +/- '+str(valuej[i].uncertainty)
+	print(printline)
+
